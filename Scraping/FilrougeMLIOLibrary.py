ae97f4517ebab68500be9d1c81c6dbb7259ec314
pchevarier <pchevarier@gmail.com>
2022-02-14 09:57:57 +0100

    rename libray + scrapping intermediaire

---
+++ Scraping/FilrougeMLIOLibrary.py
@@ -0,0 +1,66 @@
+#FilrougeML IO Librairy
+# Dataframe <-> Json
+# Structure du DataFrame
+#   TitLe: titre de l'article STRING
+#   Abstract: Résumé STRING
+#   Body: contenu (corps) de l'article STRING
+#   RefTo: Liste des DOI auquel se réfere l'article https://doi.org/leDOI
+#   Authors DICTIONNAIRE des auteurs clef: Name, Id, Organisation
+#   RefBy: List des DOI  des articles se référant à cet article
+#   DatePub: Date de publication datetime64
+#   url: URL de l'article: STRING
+#   LibName: Nom de la bibliothéque ( site web) ou est stocké l'article STRING
+import pandas as pd
+import json
+
+gorient = 'columns'
+def LoadArticles(dirname,fname):
+    """lit et Charge le fichier json dont le nom est fname dans le répertoire dirname
+        retourne le dataframe chargé
+    La varialbe globale gorient commune a LoadArticle et SaveArticle 
+    assure l'homogénéité de strucure entre lecture et écriture"""
+    df = pd.read_json(dirname+fname,orient=gorient)
+    return(df)  
+
+def SaveArticles(dirname,fname,df):
+    """Sauvegarde le dataframe d'articles df 
+    dans le fichier fname dans le répertoire dirname"""
+    df.to_json(dirname+fname,orient=gorient,date_format='iso')
+    return()
+
+def AddArticles(DOI,df=None,Title='',Abstract='',Body='',RefTo='',Authors='',RefBy='',DatePub='',url='',LibName=''):
+    """Ajoute un article dans un dataframe typé et compatible pour sauvegarde
+    Exemple d'utilisation:
+    Premier appel de dataframe n'existe pas
+    art = AddArticles('10.1128/MCB.22.9.2918-2927.2002',None,'Titre','abstract','body',[['10.1128/MCB.22.9.2918-2927.2002']],{'Name':'nomaut1','Id':'idaut1','Organization':'orgaut1'},[['10.1128/MCB.22.9.2918-2927.2002']],'2020 Feb 20','url','bib')
+    Appel suivants, le datframe existe et est passé en parametre
+    art = AddArticles('10.1rrr8/MCB.22.9.2918-2927.2002',art,'Titre2','abstract2','body2',[['22210.1128/MCB.22.9.2918-2927.2002']],{'Name':'nomaut2','Id2':'idaut2','Organization2':'orgaut21'},[['12220.1128/MCB.22.9.2918-2927.2002']],'2022 Feb 20','url2','bib2')
+    """
+    if isinstance(df,type(None)):
+        df = pd.DataFrame([[Title,Abstract,Body,RefTo,Authors,RefBy,DatePub,url,LibName]],
+                  columns = ['Title','Abstract','Body','RefTo','Authors','RefBy','DatePub','url','LibName'],
+                  index=[DOI]
+                  )
+        df= df.astype({'Title':'string','Abstract':'string','Body':'string','DatePub':'datetime64[ns]','url':'string','LibName':'string'})
+    else:
+        df.loc[DOI]= [Title,Abstract,Body,RefTo,Authors,RefBy,DatePub,url,LibName]
+    return(df)
+
+art = AddArticles('10.1128/MCB.22.9.2918-2927.2002',None,'Titre','abstract','body',[['10.1128/MCB.22.9.2918-2927.2002']],{'Name':'nomaut1','Id':'idaut1','Organization':'orgaut1'},[['10.1128/MCB.22.9.2918-2927.2002']],'2020 Feb 20','url','bib')
+art = AddArticles('10.1rrr8/MCB.22.9.2918-2927.2002',art,'Titre2','abstract2','body2',[['22210.1128/MCB.22.9.2918-2927.2002']],{'Name':'nomaut2','Id2':'idaut2','Organization2':'orgaut21'},[['12220.1128/MCB.22.9.2918-2927.2002']],'2022 Feb 20','url2','bib2')
+
+#art = pd.DataFrame([['Titre','abstract','body',[['10.1128/MCB.22.9.2918-2927.2002']],{'Name':'nomaut1','Id':'idaut1','Organization':'orgaut1'},[['10.1128/MCB.22.9.2918-2927.2002']],'2022-12-31','url','bib'],
+#                   ['Titre2','abstract2','body2',[['10.1128/MCB.22.9.2918-2927.2002']],{'Name':'nomaut2','Id':'idaut2','Organization':'orgaut2'},[['10.1128/MCB.22.9.2918-2927.2002']],'2022-11-30','url2','bib2']],
+#                  columns = ['Title','Abstract','Body','RefTo','Authors','RefBy','DatePub','url','LibName'],
+#                  index=['10.1128/MCB.22.9.2918-2927.2002','10.1128/MCB.22.9.2918-2927.2022']
+#                  )
+art= art.astype({'Title':'string','Abstract':'string','Body':'string','DatePub':'datetime64[ns]','url':'string','LibName':'string'})
+print(art)                  
+art.info()
+dirname=''
+fname = 'test.json'
+SaveArticles(dirname,fname,art)
+art2= LoadArticles(dirname,fname)
+art2= art2.astype({'Title':'string','Abstract':'string','Body':'string','DatePub':'datetime64[ns]','url':'string','LibName':'string'})
+art2.info()
+print(art2)
--- Scraping/testpch.py
+++ Scraping/testpch.py
@@ -1,6 +1,7 @@
 import requests
 import re
 from bs4 import BeautifulSoup as bs
+import FilrougeMLIOLibrary as MLOI
 BaseURL = "https://pubmed.ncbi.nlm.nih.gov/"
 DetailURL = "32087349/"
 
@@ -31,20 +32,21 @@ def ourGet(BaseURL,DetailURL,abortonerror=False):
         return(bs(r.text, 'html.parser'))
 
 Scrapdicts = {'PubMed':{
-    'BaseURL':"https://pubmed.ncbi.nlm.nih.gov/",
+    'DatePub':{'tag':'span','args':{'class': "secondary-date"}},
+    'DOI':{'tag':'span','args':{'class': "citation-doi"}},
     'Title':{'tag':'h1'},
     'Abstract':{'tag':'div','args':{'class': "abstract-content selected"}},
     'Authors':{'multi':True,'tag':'span','args':{'class': "authors-list-item"},'tag2':'a','args2':{'class': "full-name"},'ctner':'data-ga-label'},
     'OrgAuthors':{'multi':True,'tag':'span','args':{'class': "authors-list-item"},'tag2':'a','args2':{'class': "affiliation-link"},'ctner':'title'},    
-    'urlAuthors':{'multi':True,'tag':'span','args':{'class': "authors-list-item"},'tag2':'a','args2':{'class': "full-name"},'ctner':'href'}
+    'urlAuthors':{'multi':True,'tag':'span','args':{'class': "authors-list-item"},'tag2':'a','args2':{'class': "full-name"},'ctner':'href'},
+    'RefBy':{'multi':True,'tag':'div','args':{'class': "docsum-content"},'tag2':'a','args2':{'data-ga-category': "cited_by"},'ctner':'href'},   
     }}
-
-def scrapArticle(LibWeb,DetailURL,Scrapdicts):
+#DOI,df=None,Title='',Abstract='',Body='',RefTo='',Authors='',RefBy='',DatePub='',url='',LibName=''):
+def scrapArticle(LibWeb,BaseURL,DetailURL,Scrapdicts):
     
     Scrapdict = Scrapdicts[LibWeb]
-    print(Scrapdict)
-    html_paper = ourGet(Scrapdict['BaseURL'],DetailURL,True)
-
+    html_paper = ourGet(BaseURL,DetailURL,True)
+    dictres = dict()
     for ktopic,vtopic in Scrapdict.items():
         res = []
         resfa = []
@@ -57,15 +59,32 @@ def scrapArticle(LibWeb,DetailURL,Scrapdicts):
         else:   
             res.append(html_paper.find(vtopic['tag'],args))
         ret=[]
+
         for r in res:
             if r != None:
                 rr = filtretext(r.text if 'ctner' not in vtopic else r[vtopic['ctner']])
                 ret.append(rr)
-        print('ktopic',ktopic,ret)
-
+        
+        dictres[ktopic]=ret
         #print(html_paper.find_all(v['tag'],v['args']).[ctner])
+    print(dictres)
+    DOI = dictres['DOI'][0]
+    DOI = DOI.replace(' ','')
+    Title = dictres['Title']
+    AName=dictres['Authors']
+    AOrganization=dictres['OrgAuthors']
+    AUrl=dictres['urlAuthors']
+    Authors= {'Name':AName,'Organization':AOrganization,'URL':AUrl,'LibWeb':LibWeb}
+    Abstract = dictres['Abstract']
+    DatePub = dictres['DatePub'][0]
+    DatePub = DatePub.replace('Epub ','')
+    DatePub = DatePub.replace('.','')
+    RefBy=dictres['RefBy']
+    df = MLOI.AddArticles(DOI,None,Title,Abstract,'','',Authors,'',DatePub,DetailURL,LibWeb)
+    MLOI.SaveArticles("","art.json",df)
 
-scrapArticle('PubMed',DetailURL,Scrapdicts)   
+#    'BaseURL':"https://pubmed.ncbi.nlm.nih.gov/",
+scrapArticle('PubMed',BaseURL,DetailURL,Scrapdicts)   
 
 
 
